a1fc9474686f359ea275f47e7f7d3e0c7b98db50
Rolando Goloya Jr <rgoloyajr@gmail.com>
2024-06-23 14:14:20 +0800

    fix display screen sizes

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Index.css'
-import Avatar from '../../Static/Avatar.png'
+import './Index.css';
+import Avatar from '../../Static/Avatar.png';
 import styled, { css } from 'styled-components';
 import { useTheme } from 'styled-components';
 import { lightTheme, darkTheme } from '../theme/theme';
-import Resume from './resume'
+import Resume from './resume';
 
 const Avatars = styled.img`
   ${({ theme }) => {
-    const shadowColor = theme === 'light' ? lightTheme.background : darkTheme.background; // Access background property of lightTheme and darkTheme
+    const shadowColor = theme === 'light' ? lightTheme.background : darkTheme.background;
     return css`
       filter: drop-shadow(1px 1px 10px ${shadowColor});
     `;
@@ -16,15 +16,19 @@ const Avatars = styled.img`
 `;
 
 const Index = () => {
-  
   const theme = useTheme();
 
   return (
     <div id='Container'>
-      <div className="Qoute">
-        <h1>Rolando A. Goloya Jr. <br /><span className='position'>WEB DEVELOPER</span><br /><span><p>Dedicated and Passionate web developer. Eager to learn new technologies and apply ideas into real life project.</p>
-        </span>
-        </h1>
+      <div className="Quote">
+        <h2>
+          Rolando A. Goloya Jr. <br />
+          <span className='position'>WEB DEVELOPER</span>
+          <br />
+          <span>
+            <p>Dedicated and Passionate web developer. Eager to learn new technologies and apply ideas into real life project.</p>
+          </span>
+        </h2>
         <Resume />
       </div>
       <div className="Avatar">
